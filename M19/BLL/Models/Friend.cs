841430124084b361c40af4eea0b02548fc0ac4bf
Nikolyase <96522347+Nikolyase@users.noreply.github.com>
2022-04-30 18:06:36 +0300

    Добавьте файлы проекта.

---
+++ M19/BLL/Models/Friend.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SocialNetwork.BLL.Models
+{
+    public class Friend
+    {
+        public int Id { get; }
+        public string UserEmail { get; }
+        public string FriendEmail { get; }
+        public string FriendFirstName { get; }
+
+        public Friend(int id, string userEmail, string friendEmail, string friendFirstName)
+        {
+            this.Id = id;
+            this.UserEmail = userEmail;
+            this.FriendEmail = friendEmail;
+            this.FriendFirstName = friendFirstName;
+        }
+    }
+}
